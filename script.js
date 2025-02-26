d86667cf54bfe33848ea0daec79b1dc44d0b783b
lktinn <kieutien14062007@gmail.com>
2025-02-26 22:11:40 +0700

    Script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let t= tendangnhap.value
+  let m= matkhau.value
+  if (t=="dn" && m=="0108")
+  {
+    window.open("house.html")
+    document.getElementById(compiler).style.display="block"
+  }
+  }
